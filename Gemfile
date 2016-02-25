cfc09f7c302de96a7591e8a8c67314a1d6357c04
ed phillips <ed@cronos.net>
2016-02-24 20:14:25 -0800

    kitchen init

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source 'https://rubygems.org'
+
+gem 'chefspec', '~> 4.0'
+gem 'berkshelf'
+gem 'test-kitchen'
+gem 'kitchen-vagrant'
---
+++ metadata.rb
@@ -0,0 +1,15 @@
+name              'rails-test-kitchen-demo'
+version           '0.1.0'
+description       'Tests the Configure/Management of Rails deployment'
+license           'MIT License'
+source_url        'https://github.com/phosphene/rails-test-kitchen-demo'
+
+suggests 'apt'
+suggests 'yum'
+
+supports 'ubuntu'
+##supports 'debian'
+##supports 'centos'
+##supports 'redhat'
+
+##recommends 'br-ruby'
